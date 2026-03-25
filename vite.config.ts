d987bc174247bbaa48c77d24e581055e5c27d179
Hayat Khan <hayatkhan321@gmail.com>
2026-03-25 23:37:07 +0400

    Set up GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
+mport { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/DRMET/",
+  base: "/",
   plugins: [react()],
 });
